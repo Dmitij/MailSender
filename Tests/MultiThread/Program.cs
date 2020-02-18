3d9c9a47b895641768241378c9e033197556bc6d
Дмитрий Р <gorbunok_@mail.ru>
2020-02-18 13:29:05 +0300

    1. Написать приложение, считающее в раздельных потоках: a. факториал числа N, которое вводится с клавиатуру; b. сумму целых чисел до N, которое также вводится с клавиатуры.

---
+++ Tests/MultiThread/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThread
+{
+    class Program
+    {
+        static void Factorial(object n)
+        {
+            double N = Convert.ToInt64(n);
+            if (N < 0)
+                throw new ArgumentException("Значение не должно быть отрицательным");
+            double fac = 1;
+            while (N > 1)
+            {
+                Console.WriteLine("Считаю факториал");    
+                fac *= N;
+                N--;
+                Thread.Sleep(300);
+            }
+            Console.WriteLine("Факториал: " + fac);            
+        }
+        static void Sum(object n)
+        {
+            int sum = 0;
+            double N = Convert.ToInt64(n);
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Считаю сумму");                
+                sum += i;
+                Thread.Sleep(300);
+            }
+            Console.WriteLine("Сумма: " + sum);           
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N: ");
+            int n = int.Parse(Console.ReadLine());
+            // создаем новый поток
+            Thread myThread1 = new Thread(new ParameterizedThreadStart(Factorial));
+            myThread1.Start(n);
+            Thread myThread2 = new Thread(new ParameterizedThreadStart(Sum));
+            myThread2.Start(n);   
+            Console.ReadLine();
+        }
+
+       
+    }
+}
