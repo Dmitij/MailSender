9bd5d4db77ad3554577be0ec7166281a01120696
Дмитрий Р <gorbunok_@mail.ru>
2020-02-18 11:27:06 +0300

    2. Добавить UnitTest для метода getPassword проекта CodePasswordDLL. a. Методы с тестами добавить в проект CodePasswordDLL.Test класс CodePasswordTest по аналогии с тестами для метода getCodPassword. b. Добавить в класс CodePasswordTest методы TestInitialize и TestCleanup. c. Расширить метод Assert.AreEqual параметром с сообщением для output. d. Добавить Debug.WriteLine внутри тестовых методов и посмотреть, как записываются текстовые сообщения e. Добавить тестовые методы для этого же класса с использованием методов StringAssert.

--- Tests/CodePasswordDLLTest/UnitTest1.cs
+++ Tests/CodePasswordDLLTest/UnitTest1.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using MailSender.lib.Service;
+using CodePasswordDLL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Diagnostics;
 
 namespace MailSender.lib.Tests.Service
 {
@@ -35,7 +34,7 @@ namespace MailSender.lib.Tests.Service
         [TestInitialize]
         public void TestInitialize()
         {
-           Debug.WriteLine("Да начнется тест");
+
         }
 
         [TestCleanup]
@@ -57,34 +56,30 @@ namespace MailSender.lib.Tests.Service
         }
 
         [TestMethod]
-        public void Encode_ABC_to_BCD_with_key_1()
+        public void Encode_ABC_to_BCD()
         {
             // A-A-A = Arrange - Act - Assert
 
-            const string str = "ABC";
-            const int key = 1;
+            const string str = "ABC";            
             const string expected_str = "BCD";
 
-            var actual_str = TextEncoder.Encode(str, key);
+            var actual_str = CodePassword.getCodPassword(str);
 
             Assert.AreEqual(expected_str, actual_str);
         }
 
         [TestMethod]
-        public void Decode_BCD_to_ABC_with_key_1()
+        public void Decode_BCD_to_ABC()
         {
             // A-A-A = Arrange - Act - Assert
 
             const string str = "BCD";
-            const int key = 1;
-            const string expected_str = "ABC";
+            const string expected_str = "ABC1";
 
-            var actual_str = TextEncoder.Decode(str, key);
+            var actual_str = CodePassword.getPassword(str);
 
-            Assert.AreEqual(expected_str, actual_str);
+            Assert.AreEqual(expected_str, actual_str, "�������");
 
-            //StringAssert.Matches();
-            //CollectionAssert.AllItemsAreNotNull();
         }
     }
 }
